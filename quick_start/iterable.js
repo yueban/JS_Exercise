253d9a863d97ed4a9fb90ce55a50e14672e9bbb8
yueban <fbzhh007@gmail.com>
2018-02-27 22:24:09 +0800

    complete quick_start

---
+++ quick_start/iterable.js
@@ -0,0 +1,43 @@
+'use strict'
+
+// var arr = [1, 2, 3];
+// for (var i of arr) {
+//     console.log(i);
+// }
+// arr.forEach(function (element, index, array) {
+//     console.log(index + ': ' + element);
+// })
+
+// var map = new Map();
+// map.set('3', 1);
+// map.set('2', 2);
+// map.set('1', 3);
+// for (var i of map) {
+//     console.log(i);
+// }
+// map.forEach(function (value, key, map) {
+//     console.log(key + ': ' + value);
+// })
+
+// var set = new Set();
+// set.add('3');
+// set.add('2');
+// set.add('1');
+// for (var i of set) {
+//     console.log(i);
+// }
+// set.forEach(function (e, sameElem, set) {
+//     console.log(e + ': ' + sameElem);
+// });
+
+// var arr = [3, 2, 1];
+// arr.name = 'tom';
+// console.log('length:' + arr.length);
+// console.log('\nfor in:');
+// for (var i in arr) {
+//     console.log(i);
+// }
+// console.log('\nfor of:');
+// for (var i of arr) {
+//     console.log(i);
+// }
---
+++ quick_start/loop.js
@@ -0,0 +1,40 @@
+'use strict'
+
+// var result = 1;
+// for (var i = 1; i <= 10; i++) {
+//     result *= i;
+// }
+// console.log(result);
+
+
+// var arr = ['ab', 'bc', 'cd'];
+// for (var i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+// for (var key in arr) {
+//     console.log(key);
+// }
+
+// var person = {
+//     "name": "jack",
+//     "age": 19,
+//     "city": "zhengzhou"
+// }
+// for (var key in person) {
+//     if (person.hasOwnProperty(key)) {
+//         console.log(key);
+//     }
+// }
+
+// var sum = 0;
+// var counter = 100;
+// while (counter > 0) {
+//     sum += counter;
+//     counter -= 2;
+// }
+// console.log(sum);
+
+var arr = ['Bart', 'Lisa', 'Adam'];
+for (var i = 0; i < arr.length; i++) {
+    console.log(`Helle，${arr[i]}！`);
+}
